6c49eeec7a520b85cf6c773edd600903f03f9e93
KrishnaD1208 <krishnadange12@gmail.com>
2024-09-21 14:24:04 +0530

    second trial

--- 04-BasicForLoop2/for6.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-    for(int i=20;i<=50;i++){
-
-        if(i%4==3){
-            print("$i is not divisible by 4 but remainder is 3.");
-        }
-    }
-}
--- 04-BasicForLoop2/for7.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-    for(int i=20;i<=60;i++){
-        if(i%7==0){
-            print("The cube of $i : ${i*i*i}");
-        }
-    }
-}
-
